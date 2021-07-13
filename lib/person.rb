6563146b6a44c9a9f2d0d40d6e4895a02b9997fe
Devante Lowery <dlowery2120@gmail.com>
2021-07-13 13:02:59 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,34 +2,46 @@ require 'pry'
 
 
 class Person
-    #  @@all = [] 
-    # attr_accessor :name, :hygiene, :happiness, :bank_account, :salary
+    attr_accessor :bank_account, :happiness
+    attr_reader :name, :hygiene, 
+    # @@all = [] 
     
-    def initialize(name, hygiene = 8, happiness = 8, bank_account = 25)
+    
+    def initialize(name)
         @name = name
-        @hygiene = hygiene
-        @happiness = happiness
-        @bank_account = bank_account 
+        @hygiene = 8
+        @happiness = 8
+        @bank_account = 25
         # @min = min
         # @max = max
         # @@all << self
     end
-    
-    def clean
-        if @hygiene >= 7
-            "True"
-        else
-            "False"
-        end
-    end
-    def happy
-        if @happiness >= 7
-            "True"
-        else
-            "False"
+
+    def happiness=(points)
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+            
         end
     end
 
+    def clean
+       if @hygiene > 7
+            "True"
+        else
+            "False"
+        end
+    end
+    
+    def happy
+        if @happiness > 7
+            "True"
+        else
+            "False"
+        end
+    end
+# binding.pry
     def get_paid(salary)
         @bank_account = @bank_account + (salary.to_i)
         "all about the benjamins"
@@ -49,4 +61,3 @@ class Person
 end
 
 mufasa = Person.new("Kanye") 
- binding.pry
